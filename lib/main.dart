00cb1feb595c226328af7efee6b3b3b6e7659e60
Pranjal Choudhary <pranjal.choudhary2020@vitstudent.ac.in>
2023-08-21 00:24:25 +0530

    Rows and Columns widgets

--- lib/main.dart
+++ lib/main.dart
@@ -37,69 +37,87 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+      // body: const Center(
+      // child: Column(
+      // children: [
+      // Container(
+      //   height: 100,
+      //   width: 200,
+      //   color: Colors.yellow,
+      //   child: const Center(
+      //     child: Text(
+      //       "hello bois",
+      //       style: TextStyle(
+      //         fontSize: 20,
+      //         color: Colors.lightBlue,
+      //         fontWeight: FontWeight.bold,
+      //       ),
+      //     ),
+      //   ),
+      // ),
+      //   SizedBox(height: 20), // Add some spacing between the texts
+      // const Text(
+      //   "hello pranjal",
+      //   style: TextStyle(
+      //     fontSize: 18,
+      //     color: Colors.black,
+      //     fontWeight: FontWeight.normal,
+      //   ),
+      // ),
+      // TextButton(
+      //   child: const Text("click me"),
+      //   onPressed: () {
+      //     print("hello pranjal");
+      //   },
+      //   onLongPress: () => {
+      //     print("hello pranjal"),
+      //   },
+      // ),
+      // const SizedBox(height: 20),
+      // Container(
+      //   height: 10,
+      //   width: 50,
+      //   color: Colors.grey,
+      // ),
+      // ElevatedButton(
+      //     onPressed: () {
+      //       print('hi');
+      //     },
+      //     child: const Text("elevated button")),
+      // const SizedBox(height: 20),
+      // OutlinedButton(
+      //     onPressed: () => {}, child: const Text("outlined button")),
+      // const SizedBox(height: 20),
+      // Center(
+      //   child: Container(
+      //       height: 100,
+      //       width: 200,
+      //       color: Colors.lightBlue,
+      //       child: Image.asset('assets/images/logo.jpg')),
+      // ),
+
+      // ],
+      // ),
+      // ),
+      body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Container(
-              height: 100,
-              width: 200,
-              color: Colors.yellow,
-              child: const Center(
-                child: Text(
-                  "hello bois",
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.lightBlue,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-            ),
-            const SizedBox(height: 20), // Add some spacing between the texts
             const Text(
-              "hello pranjal",
-              style: TextStyle(
-                fontSize: 18,
-                color: Colors.black,
-                fontWeight: FontWeight.normal,
-              ),
+              'a',
+              style: TextStyle(fontWeight: FontWeight.bold),
             ),
-            TextButton(
-              child: const Text("click me"),
-              onPressed: () {
-                print("hello pranjal");
-              },
-              onLongPress: () => {
-                print("hello pranjal"),
-              },
-            ),
-            const SizedBox(height: 20),
-            Container(
-              height: 10,
-              width: 50,
-              color: Colors.grey,
-            ),
-            ElevatedButton(
-                onPressed: () {
-                  print('hi');
-                },
-                child: const Text("elevated button")),
-            const SizedBox(height: 20),
-            OutlinedButton(
-                onPressed: () => {}, child: const Text("outlined button")),
-            const SizedBox(height: 20),
-            Center(
-              child: Container(
-                  height: 100,
-                  width: 200,
-                  color: Colors.lightBlue,
-                  child: Image.asset('assets/images/goat.jpg')),
-            ),
-            Image.asset('assets/images/city.png'),
-          ],
-        ),
-      ),
+            const Text('b'),
+            Text('c'),
+            Text('d'),
+            ElevatedButton(onPressed: () {}, child: Text("elevated button")),
+          ]),
+      // const Row(children: [
+      //     Text('a'),
+      //     Text('b'),
+      //     Text('c'),
+      //     Text('d'),
+      //   ]),
     );
   }
 }

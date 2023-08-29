4d0e384ead164253979a0ca1e6471f68c13f25ec
Pranjal Choudhary <pranjal.choudhary2020@vitstudent.ac.in>
2023-08-29 11:56:36 +0530

    Date Picker

--- lib/date_time.dart
+++ lib/date_time.dart
@@ -9,6 +9,8 @@ class CurrDateTime extends StatefulWidget {
 }
 
 class _DateTimeState extends State<CurrDateTime> {
+  DateTime? selectedDate;
+  TimeOfDay? selectedTime;
   @override
   Widget build(BuildContext context) {
     var time = DateTime.now();
@@ -21,7 +23,70 @@ class _DateTimeState extends State<CurrDateTime> {
             onPressed: () {
               setState(() {});
             },
-            child: Text("Current Time"))
+            child: Text("Current Time")),
+        SizedBox(
+          height: 30,
+        ),
+        Text(
+          selectedDate != null
+              ? "Selected Date: ${DateFormat('yMMMMEEEEd').format(selectedDate!)}"
+              : "Select Date",
+          style: TextStyle(fontSize: 18),
+        ),
+        SizedBox(
+          height: 30,
+        ),
+        ElevatedButton(
+            onPressed: () async {
+              DateTime? datepicked = await showDatePicker(
+                  context: context,
+                  initialDate: DateTime.now(),
+                  firstDate: DateTime(1999),
+                  lastDate: DateTime(2024));
+              if (datepicked != null) {
+                setState(() {
+                  selectedDate = datepicked; // Update the selected date
+                });
+                print(datepicked);
+                print(
+                    'Date -> ${datepicked.day}/${datepicked.month}/${datepicked.year}');
+                print(
+                    "New Date -> ${DateFormat('yMMMMEEEEd').format(datepicked)}");
+              }
+            },
+            child: Text("Date")),
+        SizedBox(
+          height: 30,
+        ),
+        Text(
+          selectedTime != null
+              ? "Selected Time: ${selectedTime!.hour}:${selectedTime!.minute}"
+              : "Select Time",
+          style: TextStyle(fontSize: 18),
+        ),
+        SizedBox(
+          height: 30,
+        ),
+        ElevatedButton(
+            onPressed: () async {
+              TimeOfDay? timepicked = await showTimePicker(
+                  context: context,
+                  initialTime: TimeOfDay.now(),
+                  initialEntryMode: TimePickerEntryMode.input);
+              if (timepicked != null) {
+                setState(() {
+                  selectedTime = timepicked;
+                });
+
+                print(timepicked);
+                if (selectedDate != null) {
+                  print(
+                      "timeee -> ${selectedDate!.day}/${selectedDate!.month}/${selectedDate!.year}");
+                }
+                print("Time -> ${timepicked.hour}:${timepicked.minute}");
+              }
+            },
+            child: Text("Time")),
       ],
     );
   }
